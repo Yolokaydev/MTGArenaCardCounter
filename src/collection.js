254ddc7f8393da01de7e7484fcecc5bdf1957011
unknown <laszlokokaidev@gmail.com>
2024-06-24 21:13:39 +0200

    refractoring

--- src/collection.js
+++ src/collection.js
@@ -1,26 +1,21 @@
+import Card from "./card.js";
+
 export default class Collection {
   constructor() {
     this.cards = [];
-    this.collectionInfo = new collectionInfo();
   }
 
   addCard(card) {
     this.cards.push(card);
   }
+
   count() {
     return this.cards.length;
   }
 
   getCollectionInfo() {
-    this.cards.forEach((cardEntry) => {});
-  }
-}
-
-class collectionInfo {
-  uniqueCardsCount;
-  allCardsCount;
-
-  constructor() {
-    // console.log("collectionInfo created");
+    this.cards.forEach((cardEntry) => {
+      console.log(cardEntry);
+    });
   }
 }
